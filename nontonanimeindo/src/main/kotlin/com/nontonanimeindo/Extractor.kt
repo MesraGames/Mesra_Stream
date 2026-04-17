21999f580ee6c7b30cee99532bf10564ac4f4d2e
MesraGames <eminemjie123@gmail.com>
2026-04-17 23:31:35 +0800

    Fix

--- nontonanimeindo/src/main/kotlin/com/nontonanimeindo/Extractor.kt
+++ nontonanimeindo/src/main/kotlin/com/nontonanimeindo/Extractor.kt
@@ -1,13 +1,7 @@
 package com.nontonanimeindo
 
 import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.extractors.Vidstack
 import com.lagradost.cloudstream3.extractors.StreamWishExtractor
-
-class VidStackExtractor : Vidstack() {
-    override val name = "VidStack"
-    override val mainUrl = "https://vidstack.com"
-}
 
 class MyStreamWish : StreamWishExtractor() {
     override val name = "StreamWish"
