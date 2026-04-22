871409eba8fe1c9098b572ea48ca66ecf1f4fe00
MesraGames <101082318+MesraGames@users.noreply.github.com>
2026-04-21 23:57:15 -0700

    Delete napus directory

--- napus/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-apply(plugin = "com.android.library")
-apply(plugin = "kotlin-android")
-apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-dependencies {
-    // Tambahkan dependensi khusus ekstensi di sini jika diperlukan
-}
