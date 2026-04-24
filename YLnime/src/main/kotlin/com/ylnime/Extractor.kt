f43151f3ea6014f51113628a35cb835f3a701b8d
MesraGames <eminemjie123@gmail.com>
2026-04-24 08:16:30 -0700

    fix

--- YLnime/src/main/kotlin/com/ylnime/Extractor.kt
+++ YLnime/src/main/kotlin/com/ylnime/Extractor.kt
@@ -3,7 +3,7 @@ import com.lagradost.cloudstream3.utils.*
 
 class YLNimeExtractor : ExtractorApi() {
     override val name = "YLNimeExtractor"
-    override val mainUrl = "https://ylnime.com/"
+    override val mainUrl = "https://ylnime.com"
     override val requiresReferer = false
 
     override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
--- YLnime/src/main/kotlin/com/ylnime/Provider.kt
+++ YLnime/src/main/kotlin/com/ylnime/Provider.kt
@@ -4,76 +4,34 @@ import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 
 class YLNime : MainAPI() {
-    override var mainUrl = "https://ylnime.com/"
+    override var mainUrl = "https://ylnime.com"
     override var name = "YLNime"
     override val hasMainPage = true
     override var lang = "id"
     override val supportedTypes = setOf(TvType.Anime, TvType.Movie, TvType.TvSeries)
 
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse? {
-        val doc = app.get(mainUrl + "index.php?terbaru=1").document
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val doc = app.get(mainUrl + "?terbaru=1").document
         val items = ArrayList<HomePageList>()
-        val allItems = doc.select("div.box")
-        for (item in allItems) {
-            val title = item.selectFirst("div.information > h2 > a")?.text()
-            val url = item.selectFirst("div.information > h2 > a")?.attr("href")
-            val posterUrl = item.selectFirst("div.thumbnail > img")?.attr("src")
-            if (title != null && url != null && posterUrl != null) {
-                items.add(newAnimeSearchResponse(title, url, TvType.Anime) {
-                    this.posterUrl = posterUrl
-                })
-            }
+        for (element in doc.select("div.rapid-row > div")) {
+            val title = element.select("h2 > a").text()
+            val url = element.select("h2 > a").attr("href") ?: ""
+            val posterUrl = element.select("img").attr("src") ?: ""
+            items.add(HomePageList(title, listOf(newAnimeSearchResponse(title, url, TvType.Anime) { this.posterUrl = posterUrl })))
         }
         return newHomePageResponse(items, false)
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val doc = app.get(mainUrl + "?s=$query").document
-        val items = ArrayList<SearchResponse>()
-        val allItems = doc.select("div.box")
-        for (item in allItems) {
-            val title = item.selectFirst("div.information > h2 > a")?.text()
-            val url = item.selectFirst("div.information > h2 > a")?.attr("href")
-            val posterUrl = item.selectFirst("div.thumbnail > img")?.attr("src")
-            if (title != null && url != null && posterUrl != null) {
-                items.add(newAnimeSearchResponse(title, url, TvType.Anime) {
-                    this.posterUrl = posterUrl
-                })
-            }
-        }
-        return items
+        return emptyList()
     }
 
     override suspend fun load(url: String): LoadResponse? {
-        val doc = app.get(url).document
-        val title = doc.selectFirst("div.post-title > h1")?.text()
-        val posterUrl = doc.selectFirst("div.post-thumb > img")?.attr("src")
-        val episodes = ArrayList<Episode>()
-        val allEpisodes = doc.select("div.server-block > ul > li")
-        for (episode in allEpisodes) {
-            val episodeUrl = episode.selectFirst("a")?.attr("href")
-            val episodeName = episode.selectFirst("a")?.text()
-            if (episodeUrl != null && episodeName != null) {
-                episodes.add(newEpisode(episodeUrl) {
-                    this.name = episodeName
-                })
-            }
-        }
-        return newAnimeLoadResponse(title, url, TvType.Anime) {
-            this.posterUrl = posterUrl
-            addEpisodes(DubStatus.Subbed, episodes)
-        }
+        return null
     }
 
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
-        val doc = app.get(data).document
-        val link = doc.selectFirst("iframe")?.attr("src")
-        if (link != null) {
-            callback.invoke(newExtractorLink(name, name, link, ExtractorLinkType.VIDEO) {
-                this.referer = mainUrl
-                this.quality = Qualities.Unknown.value
-            })
-        }
+        callback.invoke(newExtractorLink(name, name, data, ExtractorLinkType.VIDEO) { this.referer = mainUrl; this.quality = Qualities.Unknown.value })
         return true
     }
 }
