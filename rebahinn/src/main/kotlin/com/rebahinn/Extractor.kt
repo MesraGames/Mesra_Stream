e3b561a1f802d9fc22df5234af69bfe6e1acac70
MesraGames <eminemjie123@gmail.com>
2026-04-30 19:52:48 -0700

    delete plugin

--- rebahinn/src/main/kotlin/com/rebahinn/Extractor.kt
+++
@@ -1,9 +0,0 @@
-package com.rebahinn
-
-import com.lagradost.cloudstream3.utils.ExtractorApi
-
-class RebahinnExtractor : ExtractorApi() {
-    override var name = "Rebahinn"
-    override var mainUrl = "https://rebahinn.net"
-    override val requiresReferer = false
-}
