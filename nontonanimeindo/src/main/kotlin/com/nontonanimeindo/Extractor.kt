c13f90002eb1ef03085e71c51514b11e9e0b93be
MesraGames <eminemjie123@gmail.com>
2026-05-03 22:39:20 -0700

    delete plugin

--- nontonanimeindo/src/main/kotlin/com/nontonanimeindo/Extractor.kt
+++
@@ -1,9 +0,0 @@
-package com.nontonanimeindo
-
-import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.extractors.StreamWishExtractor
-
-class MyStreamWish : StreamWishExtractor() {
-    override val name = "StreamWish"
-    override val mainUrl = "https://streamwish.to"
-}
