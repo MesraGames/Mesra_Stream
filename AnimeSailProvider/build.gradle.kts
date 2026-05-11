1a8a520d47c2600066e5356cc8e0d13578947399
MesraGames <eminemjie123@gmail.com>
2026-05-11 00:35:12 -0700

    fix plugin

--- AnimeSailProvider/build.gradle.kts
+++ AnimeSailProvider/build.gradle.kts
@@ -1,13 +1,13 @@
 // use an integer for version numbers
-version = 6
+version = 2
 
 
 cloudstream {
     language = "id"
     // All of these properties are optional, you can safely remove them
 
-    description = "AnimeSail — Streaming Anime, Donghua Subtitle Indonesia"
-     authors = listOf("Miku")
+    // description = "Lorem Ipsum"
+     authors = listOf("Hexated, Duro92")
 
     /**
      * Status int as the following:
@@ -23,5 +23,5 @@ cloudstream {
         "OVA",
     )
 
-    iconUrl = "https://aghanim.xyz/wp-content/themes/animesail/assets/images/ico.png"
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://154.26.137.28&size=%size%"
 }
