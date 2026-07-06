4739571c94d3f8b422ff5b0283a1f1e120cae954
MesraGames <eminemjie123@gmail.com>
2026-07-06 01:41:30 -0700

    Add Plugin

---
+++ SaikoNime/saikonime.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    language = "id"
+    description = "Nonton Anime dan Donghua Subtitle Indonesia"
+    authors = listOf("RavenX")
+    status = 1
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie",
+        "OVA",
+    )
+    iconUrl = "https://www.google.com/s2/favicons?domain=nontonanimex.com&sz=128"
+}
